40f59a59a32354589b095c06eb8229eff63a0cd0
SmartFlow Developer <adekunle@smartflow.dev>
2026-01-14 20:19:13 +0100

    feat: add UI components barrel export

---
+++ frontend/components/ui/index.ts
@@ -0,0 +1,9 @@
+export { Button } from "./Button";
+export { Input } from "./Input";
+export { Textarea } from "./Textarea";
+export { Card } from "./Card";
+export { Badge } from "./Badge";
+export { Avatar } from "./Avatar";
+export { Spinner } from "./Spinner";
+export { Skeleton, SignatureCardSkeleton, StatCardSkeleton } from "./Skeleton";
+export { Tooltip } from "./Tooltip";
