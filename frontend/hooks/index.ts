067edfc0c3f4ffff7f0aba726fa57ac1dc4d4430
SmartFlow Developer <adekunle@smartflow.dev>
2026-01-14 20:51:25 +0100

    feat: export all custom hooks from index

--- frontend/hooks/index.ts
+++ frontend/hooks/index.ts
@@ -1,1 +1,7 @@
 export { useFrameAutoConnect } from "./useFrameAutoConnect";
+export { usePlatformStats } from "./usePlatformStats";
+export { useUserStats } from "./useUserStats";
+export { useUserBadges } from "./useUserBadges";
+export { useGuestbook } from "./useGuestbook";
+export { useCopyToClipboard } from "./useCopyToClipboard";
+export { useSignGuestbook } from "./useSignGuestbook";
