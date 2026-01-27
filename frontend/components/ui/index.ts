78ee58aa0a48ce5b900447a2326fd76ecd067e3f
AdekunleBamz <bams.kunle@gmail.com>
2026-01-27 15:22:52 +0100

    feat(exports): add all V2 component exports to ui/index.ts

--- frontend/components/ui/index.ts
+++ frontend/components/ui/index.ts
@@ -51,3 +51,37 @@ export { StatusIndicator } from "./StatusIndicator";
 // Utility components
 export { IconButton } from "./IconButton";
 export { CopyToClipboard, CopyField } from "./CopyToClipboard";
+
+// V2 Enhanced Components
+export { AnimatedCounter, CountUpOnView } from "./AnimatedCounter";
+export { Confetti, useConfetti } from "./Confetti";
+export { RankBadge, RankChange, LeaderboardRank } from "./RankBadge";
+export { AchievementUnlock, useAchievementNotifications } from "./AchievementUnlock";
+export { FloatingActionButton, SpeedDial, FABAction } from "./FloatingActionButton";
+export { BottomSheet, useBottomSheet } from "./BottomSheet";
+export { Stepper, Step, useStepper } from "./Stepper";
+export { Timeline, TimelineItem, ActivityTimeline } from "./Timeline";
+export { ShareSheet, useShare } from "./ShareSheet";
+export { QRCode, AddressQRCode, ShareableQRCode } from "./QRCode";
+export { ReactionButton, ReactionPicker, useReactions } from "./ReactionButton";
+export { PopoverMenu, ContextMenu, MoreOptions } from "./PopoverMenu";
+export { PullToRefresh, InfiniteScroll, VirtualList, Swipeable } from "./PullToRefresh";
+export { ProgressBar, CircularProgressV2, StepProgress } from "./ProgressBar";
+export { Heatmap, ActivityHeatmap, StreakHeatmap } from "./Heatmap";
+export { BarChart, LineChart, DonutChart, SparkLine } from "./Charts";
+export { TransactionModal, useTransactionSteps, TransactionToast, GasEstimate } from "./TransactionModal";
+export { NetworkBadge, NetworkSwitcher, WrongNetworkBanner, NetworkStatus } from "./NetworkBadge";
+export { SearchBar, SearchWithSuggestions, useSearch, AddressSearch } from "./SearchBar";
+export { FilterChips, FilterChipsWithClear, BadgeFilter, RarityFilter, TimeRangeFilter, StatusFilter, useFilters, ActiveFilters } from "./FilterChips";
+export { SortDropdown, LeaderboardSort, GuestbookSort, useSort, SortButton, TableSort } from "./SortDropdown";
+export { Input as InputV2, TextArea, Select as SelectV2, Checkbox as CheckboxV2, Radio } from "./FormElements";
+export { toast, ToastContainer, txToast, NotificationBadge } from "./ToastSystem";
+export { EmptyState as EmptyStateV2, NoSignaturesEmpty, NoMessagesEmpty, NoBadgesEmpty, NoResultsEmpty, NoWalletEmpty, NoActivityEmpty, ErrorState as ErrorStateV2, NotFoundState, MaintenanceState, OfflineState } from "./EmptyErrorStates";
+export { LoadingOverlay, LoadingSpinner, LoadingDots, LoadingPulse, SkeletonCard, SkeletonList, SkeletonTable, LoadingButton, PageLoader, useLoading } from "./LoadingStates";
+export { StatCard as StatCardV2, StatGrid, CounterStat, StatComparison, LeaderboardStat, MiniStat, StreakStat } from "./StatCards";
+export { AvatarV2, AvatarGroupV2, AvatarWithNameV2, AddressAvatarV2, AvatarUploadV2 } from "./AvatarV2";
+export { ParallaxContainer, MouseParallax, FadeIn, SlideIn, ScaleIn, Stagger, Pulse, Floating, TypeWriter, CountUp } from "./Animations";
+export { Card as CardV2, CardHeader, CardContent, CardFooter, InteractiveCard, FeatureCardV2, SignatureCardV2, ActionCard, MetricCard } from "./Cards";
+export { AlertBanner, InlineAlert, InfoBox as InfoBoxV2, Callout, AnnouncementBar, StatusIndicator as StatusIndicatorV2 } from "./AlertsNotices";
+export { OptimizedImage, NFTImage, ImageGallery, PlaceholderImage } from "./ImageComponents";
+export { DropdownMenu, DropdownItem, DropdownDivider, DropdownLabel, Accordion as AccordionV2, Breadcrumbs, Pagination as PaginationV2, usePagination } from "./Navigation";
